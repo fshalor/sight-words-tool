4b25ff1fd1514626bfe02f25a9a8369386d1abc4
Ken Decoteau <fshalor@gmail.com>
2020-09-09 08:06:04 -0400

    Testing

---
+++ app/testapp.py
@@ -0,0 +1,14 @@
+# app.py
+
+from flask import Flask
+app = Flask(__name__)
+app.config.SERVER_NAME = ""
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
